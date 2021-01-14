91c79d732c6dd0fc02073ff9e3cdc921c62c6722
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2021-01-14 02:59:42 +0000

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 //your code here
+import React, { Component} from 'react';
+
+export default class CommentComponent extends Component {
+  render() {
+    return (
+      <div className="bar" id="comment">
+
+      </div>
+    );
+  }
+}
